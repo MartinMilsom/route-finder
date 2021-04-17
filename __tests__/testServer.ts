0b3a99bbbfc1702cc5ad85e6e1b8ca67c12e5f2e
Martin Milsom <milsommartin@gmail.com>
2021-04-17 20:00:35 +0100

    extract file

---
+++ __tests__/testServer.ts
@@ -0,0 +1,29 @@
+import { ApolloServer, makeExecutableSchema } from "apollo-server-micro";
+import { createTestClient } from 'apollo-server-testing';
+
+export async function testServer(data: any, find: jest.Mock<any, any>) {
+  find.mockReturnValue({
+    toArray: () => Promise.resolve(data)
+  });
+
+  const dbContext = {
+    db: {
+      collection: () => {
+        return { find };
+      }
+    }
+  };
+
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+
+  return createTestClient(
+    new ApolloServer({
+      schema,
+      context: () => { return dbContext; }
+    }
+    )
+  );
+}
