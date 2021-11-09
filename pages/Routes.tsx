aafeee005b802a30e8ea2677d69e5cf437101e3e
Martin Milsom <martin.milsom@corlife.com>
2021-11-09 17:44:44 +0000

    card for displaying routes

--- pages/Routes.tsx
+++ pages/Routes.tsx
@@ -1,5 +1,7 @@
 import { Fragment, FunctionComponent } from "react";
 import { Route } from "../types/domain/Route";
+import { Box, Card, CardBody, CardFooter, Button, Text, Grid } from "grommet";
+import { Link as LinkIcon } from "grommet-icons";
 
 
 interface RouteListProps {
@@ -9,13 +11,33 @@ interface RouteListProps {
 export const Routes: FunctionComponent<RouteListProps> = ({ routes }) => {
     const heading = routes?.length > 0 ? <h2>Walks</h2>: "";
 
+    console.log(routes)
     return <Fragment>
         {heading}
-        <ul>
+        <Box fill>
+            <Grid
+                gap="small"
+                pad={{
+                    vertical: "large"
+                }}
+                rows={routes.map(x => "xsmall")}>
             {routes?.map((route, index) => (
-                <li key={index}><a href={route.originalLink}>{route.name}</a></li>
+                <Card background="light-1">
+                    <CardBody pad="small">
+                        <Box direction="row">
+                            <Box pad={{horizontal: "medium"}}><Text>{route.name}</Text></Box>
+                            <Box pad={{horizontal: "medium"}}><Text>{route.direction}</Text></Box>
+                            <Box pad={{horizontal: "medium"}}><Text>{route.distance}</Text></Box>
+                            <Box pad={{horizontal: "medium"}}><Text>{route.county}</Text></Box>
+                        </Box>  
+                    </CardBody>
+                    <CardFooter pad="xxxsmall" background="light-2">
+                        <Button href={route.originalLink} icon={<LinkIcon color="plain" />} hoverIndicator />
+                    </CardFooter>
+                </Card>
             ))}
-        </ul>
+            </Grid>
+        </Box>
     </Fragment>;
 };
 
